8d2575db44d9e771bd3b8d498dd6de42275e382f
Karoflow <karoflow.work@gmail.com>
2025-10-23 17:17:00 +0330

    1 - Hello World

---
+++ 1-hello-world/index.js
@@ -0,0 +1,15 @@
+console.log('Hello World')
+
+function sayHello(name){
+    return `Hello ${name}`
+}
+
+function addthree(n){
+    return n+3
+}
+
+console.log(
+    sayHello('Karo'),
+    '18 + 3 = ',
+    addthree(18),
+)
