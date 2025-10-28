617cd79ef3d7a7845832e0400f7f436d91fbb48c
Karoflow <karoflow.work@gmail.com>
2025-10-28 12:19:14 +0330

    update contact list: show menu

--- contact-list/index.js
+++ contact-list/index.js
@@ -1,25 +1,60 @@
 import readline from 'readline/promises';
 import {stdin as input, stdout as output} from 'process';
 
-const rl = readline.createInterface({input,output});
+const rl = readline.createInterface({input, output});
 
 const contactsList = [];
+
 // const in this line help us to fix type of variable
 
-const firstName = await rl.question('First name: ');
-const lastName = await rl.question('Last name: ');
 
-const newContact = {
+async function help() {
+  console.log('a: add new contact\ns: show contacts list\nq: quit');
+  let action = await rl.question('Enter your input: ');
+
+  switch (action) {
+    case 'a':
+      await addNewContact();
+      break;
+    case 's':
+      showContactsList();
+      break;
+    case 'q':
+      quit()
+      return;
+  }
+  await help();
+};
+
+
+async function addNewContact() {
+  const firstName = await rl.question('First name: ');
+  const lastName = await rl.question('Last name: ');
+
+  const newContact = {
     id: contactsList.length,
     firstName,
     lastName,
+  };
+
+  contactsList.push(newContact);
 };
 
-contactsList.push(newContact);
-const formattedContactsList =
+
+function showContactsList() {
+  const formattedContactsList =
     contactsList.map(({id, firstName, lastName}) => `#${id} ${firstName} ${lastName}`)
-        .join('\n');
+      .join('\n');
 
-console.log('Contact List: ', formattedContactsList);
+  console.log('Contact List:\n' + formattedContactsList);
+};
 
-rl.close()
+
+function quit() {
+  rl.close()
+};
+
+await help()
+// await addNewContact();
+// showContactsList();
+// quit();
